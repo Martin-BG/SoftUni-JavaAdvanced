75c48784803f3e6a79a393b0a443027c2a6b1697
MartinBG <MartinBG@abv.bg>
2017-09-20 03:55:49 +0300

    Solutions for problems 3 - 8

--- src/Lesson01IntroToJava/Lab/Pr08Lottery.java
+++ src/Lesson01IntroToJava/Lab/Pr08Lottery.java
@@ -7,10 +7,8 @@ public class Pr08Lottery {
 
         for (int i = 1; i <= endNumber - 2; i++) {
             for (int j = i + 1; j <= endNumber - 1; j++) {
-                for (int k = j; k <= endNumber; k++) {
-                    if (i != j && i != k && j != k) {
-                        System.out.printf("%d %d %d%n", i, j, k);
-                    }
+                for (int k = j + 1; k <= endNumber; k++) {
+                    System.out.printf("%d %d %d%n", i, j, k);
                 }
             }
         }
