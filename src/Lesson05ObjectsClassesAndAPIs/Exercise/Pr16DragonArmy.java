2680c948445bcede59949a2c16391dfc13e111b2
MartinBG <MartinBG@abv.bg>
2017-10-07 18:41:59 +0300

    Refactoring

--- src/Lesson05ObjectsClassesAndAPIs/Exercise/Pr16DragonArmy.java
+++ src/Lesson05ObjectsClassesAndAPIs/Exercise/Pr16DragonArmy.java
@@ -10,7 +10,10 @@ import java.util.TreeMap;
 public class Pr16DragonArmy {
 
     public static void main(String[] args) throws IOException {
-        Map<String, Map<String, Double[]>> dragons = new LinkedHashMap<>();
+        final int DEFAULT_HEALTH = 250;
+        final int DEFAULT_DAMAGE = 45;
+        final int DEFAULT_ARMOR = 10;
+        Map<String, Map<String, int[]>> dragons = new LinkedHashMap<>();
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
         int dragonsToRead = Integer.parseInt(reader.readLine());
@@ -18,19 +21,19 @@ public class Pr16DragonArmy {
             String[] tokens = reader.readLine().split("\\s+");
             String type = tokens[0];
             String name = tokens[1];
-            double damage = "null".equals(tokens[2]) ? 45d : Double.parseDouble(tokens[2]);
-            double health = "null".equals(tokens[3]) ? 250d : Double.parseDouble(tokens[3]);
-            double armor = "null".equals(tokens[4]) ? 10d : Double.parseDouble(tokens[4]);
+            int damage = "null".equals(tokens[2]) ? DEFAULT_DAMAGE : Integer.parseInt(tokens[2]);
+            int health = "null".equals(tokens[3]) ? DEFAULT_HEALTH : Integer.parseInt(tokens[3]);
+            int armor = "null".equals(tokens[4]) ? DEFAULT_ARMOR : Integer.parseInt(tokens[4]);
 
             if (!dragons.containsKey(type)) {
                 dragons.put(type, new TreeMap<>());
             }
 
-            dragons.get(type).put(name, new Double[]{damage, health, armor});
+            dragons.get(type).put(name, new int[]{damage, health, armor});
         }
 
         dragons.forEach((type, members) -> {
-            final Double[] avgStats = {0d, 0d, 0d};
+            final double[] avgStats = new double[3];
             members.values().forEach(x -> avgStats[0] += x[0]);
             members.values().forEach(x -> avgStats[1] += x[1]);
             members.values().forEach(x -> avgStats[2] += x[2]);
@@ -41,9 +44,8 @@ public class Pr16DragonArmy {
             System.out.printf("%s::(%.2f/%.2f/%.2f)%n", type, avgStats[0], avgStats[1], avgStats[2]);
 
             members.forEach((name, stats) ->
-                    System.out.printf("-%s -> damage: %.0f, health: %.0f, armor: %.0f%n",
+                    System.out.printf("-%s -> damage: %d, health: %d, armor: %d%n",
                             name, stats[0], stats[1], stats[2]));
         });
-        System.out.println();
     }
 }
