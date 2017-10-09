b794ace273c31acd68186c9eb419e3beef8f5266
MartinBG <MartinBG@abv.bg>
2017-10-10 02:11:01 +0300

    placeholders

---
+++ src/Lesson07BuiltInQueryMethodsStreamAPI/Exercise/Pr01StudentsByGroup.java
@@ -0,0 +1,8 @@
+package Lesson07BuiltInQueryMethodsStreamAPI.Exercise;
+
+public class Pr01StudentsByGroup {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Lesson07BuiltInQueryMethodsStreamAPI/Lab/Pr01TakeTwo.java
@@ -0,0 +1,8 @@
+package Lesson07BuiltInQueryMethodsStreamAPI.Lab;
+
+public class Pr01TakeTwo {
+
+    public static void main(String[] args) {
+
+    }
+}
