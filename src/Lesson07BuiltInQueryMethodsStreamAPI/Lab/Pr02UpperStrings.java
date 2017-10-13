2f93f93c0d4ac89a9197703591f9ad79cf21e253
MartinBG <MartinBG@abv.bg>
2017-10-14 00:10:46 +0300

    Lab solutions

--- src/Lesson07BuiltInQueryMethodsStreamAPI/Lab/Pr02UpperStrings.java
+++ src/Lesson07BuiltInQueryMethodsStreamAPI/Lab/Pr02UpperStrings.java
@@ -5,14 +5,11 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
-public class Pr01TakeTwo {
+public class Pr02UpperStrings {
 
     public static void main(String[] args) throws IOException {
         Arrays.stream((new BufferedReader(new InputStreamReader(System.in)).readLine().split("\\s+")))
-                .mapToInt(Integer::parseInt)
-                .filter(x -> (x >= 10) && (x <= 20))
-                .distinct()
-                .limit(2)
+                .map(String::toUpperCase)
                 .forEach(n -> System.out.print(n + " "));
     }
 }
