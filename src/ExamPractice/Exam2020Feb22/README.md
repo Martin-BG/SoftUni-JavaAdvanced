22a964274717d1e7d7be2998f43d12c710fc02fe
MartinBG <MartinBG@abv.bg>
2020-04-02 04:58:27 +0300

    03. Guild from Java Advanced Exam - 22 Feb 2020

---
+++ src/ExamPractice/Exam2020Feb22/README.md
@@ -0,0 +1,3 @@
+# Java Advanced Exam - 22 Feb 2020
+
+[**Judge** link](https://judge.softuni.bg/Contests/Practice/Index/2036#0)
---
+++ src/ExamPractice/Exam2020Feb22/pr03Guild/guild/Rank.java
@@ -0,0 +1,10 @@
+package ExamPractice.Exam2020Feb22.pr03Guild.guild;
+
+public final class Rank {
+
+    public static final String MEMBER = "Member";
+    public static final String TRIAL = "Trial";
+
+    private Rank() {
+    }
+}
