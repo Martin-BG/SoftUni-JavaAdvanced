f65bee8a414e87fd368c80dd33d6db6235262327
MartinBG <MartinBG@abv.bg>
2020-06-30 03:27:35 +0300

    02. Archery Tournament

--- src/RandomSolutions/JavaFundamentals/MidExam2019December10/Pr02ArcheryTournament.java
+++ src/RandomSolutions/JavaFundamentals/MidExam2019December10/Pr02ArcheryTournament.java
@@ -6,8 +6,8 @@ import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.Collections;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.function.Consumer;
+import java.util.function.Predicate;
+import java.util.function.ToIntFunction;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
@@ -30,19 +30,22 @@ public class Pr02ArcheryTournament {
                 .map(Integer::parseInt)
                 .collect(Collectors.toList());
 
-        AtomicInteger points = new AtomicInteger();
+        ToIntFunction<String> commandProcessor = commandProcessor(targets);
+        Predicate<String> notGameOver = command -> !"Game over".equals(command);
 
-        reader.lines()
-                .takeWhile(command -> !"Game over".equals(command))
-                .forEachOrdered(commandProcessor(targets, points));
+        int points = reader.lines()
+                .takeWhile(notGameOver)
+                .mapToInt(commandProcessor)
+                .sum();
 
         System.out.println(targets.stream().map(String::valueOf).collect(Collectors.joining(" - ")));
-        System.out.printf("Iskren finished the archery tournament with %d points!", points.get());
+        System.out.printf("Iskren finished the archery tournament with %d points!", points);
     }
 
-    private static Consumer<String> commandProcessor(List<Integer> targets,
-                                                     AtomicInteger points) {
+    private static ToIntFunction<String> commandProcessor(List<Integer> targets) {
         return command -> {
+            int pointsScored = 0;
+
             if ("Reverse".equals(command)) {
                 Collections.reverse(targets);
             } else if (SHOOT_MATCHER.reset(command).matches()) {
@@ -50,11 +53,12 @@ public class Pr02ArcheryTournament {
                 int startIndex = Integer.parseInt(SHOOT_MATCHER.group("start"));
                 int length = Integer.parseInt(SHOOT_MATCHER.group("length"));
 
-                int newPoints = shootAtTarget(targets, direction, startIndex, length);
-                points.addAndGet(newPoints);
+                pointsScored = shootAtTarget(targets, direction, startIndex, length);
             } else {
                 // throw new IllegalArgumentException("Invalid command: " + command);
             }
+
+            return pointsScored;
         };
     }
 
